ca0da3834814c9bed473e59da1371378f47ab55b
vani8987 <vano-smole@mail.ru>
2025-02-04 00:35:25 +0700

    update test__!fix the error!

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,5 +6,5 @@ module.exports = {
     ],
     "transform": {
      "^.+\\.css$": "jest-transform-stub"
-   }
+    },
 };
--- src/Api/OpenWeatherMap/OpenWeatherApi.test.tsx
+++ src/Api/OpenWeatherMap/OpenWeatherApi.test.tsx
@@ -1,7 +1,49 @@
-;
-
-
+import HandlerApiWeather from "./OpenWeatherMap"
+import { render, screen, fireEvent } from '@testing-library/react';
 
 describe("HandlerApiWeather", () => {
-    it.todo("Написать тест для функции с апи")
+    
+    it("Getting data from the API", async () => {
+        const mockSetMethod = jest.fn()
+        const city = "шушенское"
+
+        const mockFetchResponse = {
+            ok: true,
+            status: 200,
+            json: jest.fn().mockResolvedValue({
+                main: {
+                    temp: 20,
+                },
+                weather: [{
+                    description: 'Облачно',
+                }],
+            }),
+        };
+
+        jest.spyOn(window, 'fetch').mockResolvedValue(mockFetchResponse);
+
+        await HandlerApiWeather(city, mockSetMethod)
+
+        expect(mockSetMethod).toHaveBeenCalledTimes(1)
+        expect(mockSetMethod).toHaveBeenCalledWith({
+            main: {
+              temp: 20,
+            },
+            weather: [{
+              description: 'Облачно',
+            }],
+        })
+    })
+    
+    it("error receiving data", async () => {
+        const mockSetMethod = jest.fn()
+        const city = "шушенское"
+
+        jest.spyOn(window, 'fetch').mockRejectedValue(new Error("Сетевая ошибка"));
+
+        await HandlerApiWeather(city, mockSetMethod);
+
+        expect(mockSetMethod).toHaveBeenCalledTimes(0)
+        expect(mockSetMethod).toHaveBeenCalledWith({})
+    })
 })
